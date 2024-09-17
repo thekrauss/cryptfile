af8f97ac680ec6e9a1737b769747a5ba2b152e41
vkraussl <thekrauss31@gmail.com>
2024-09-18 00:14:21 +0200

    3e commit

--- fcrypt/fcrypt.go
+++ fcrypt/fcrypt.go
@@ -5,79 +5,126 @@ import (
 	"crypto/cipher"
 	"crypto/rand"
 	"crypto/sha1"
-	"encoding/hex"
 	"io"
 	"os"
 
 	"golang.org/x/crypto/pbkdf2"
 )
 
-func Encrypt(src string, password []byte) {
-	if _, err := os.Stat(src); os.IsNotExist(err) {
-		panic(err.Error())
+// prend en entrée le chemin du fichier et un mot de passe sous forme de tableau d'octets (password).
+func Encrypt(src string, password []byte) error {
+
+	if !ValidFile(src) {
+		panic("fichier non trouver")
 	}
 
 	srcFile, err := os.Open(src)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 	defer srcFile.Close()
 
 	textFile, err := io.ReadAll(srcFile)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
-	key := password
 
+	// nonce de 12 octets pour l'algorithme AES-GCM
+	// nonce est une valeur aléatoire utilisée pour garantir que chaque chiffrement est unique.
 	nonce := make([]byte, 12)
 	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
+		return err
 	}
-	dk := pbkdf2.Key(key, nonce, 4096, 32, sha1.New)
 
+	// PBKDF2 pour dériver une clé à partir du mot de passe et du nonce
+	dk := pbkdf2.Key(password, nonce, 4096, 32, sha1.New)
+
+	// nouveau bloc de chiffrement AES en utilisant la clé dérivée
 	block, err := aes.NewCipher(dk)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 
+	// objet GCM (Galois/Counter Mode), un mode de chiffrement sécurisé pour AES
 	asegm, err := cipher.NewGCM(block)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 
+	// chiffre le texte du fichier en utilisant le nonce et AES-GCM
 	cipherText := asegm.Seal(nil, nonce, textFile, nil)
+
+	// Ajoute le nonce à la fin du texte chiffré
 	cipherText = append(cipherText, nonce...)
 
 	outFile, err := os.Create(src)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 	defer outFile.Close()
 
+	// ecrit le texte chiffré dans le fichier
 	_, err = outFile.Write(cipherText)
-	if err != nil {
-		panic(err.Error())
-	}
-
+	return err
 }
 
-func Decrypt(src string, password []byte) {
-	if _, err := os.Stat(src); os.IsNotExist(err) {
-		panic(err.Error())
+func Decrypt(src string, password []byte) error {
+
+	if !ValidFile(src) {
+		panic("fichier non trouver")
 	}
 
 	srcFile, err := os.Open(src)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 	defer srcFile.Close()
 
 	cipherText, err := io.ReadAll(srcFile)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 
-	key := password
-	salt := cipherText[len(cipherText)-12]
-	str := hex.EncodeToString(salt)
+	// recupère le nonce (les 12 derniers octets du fichier chiffré)
+	nonce := cipherText[len(cipherText)-12:]
+
+	// Sépare le texte chiffré du nonce
+	cipherText = cipherText[:len(cipherText)-12]
+
+	// Utilise PBKDF2 pour dériver la clé à partir du mot de passe et du nonce
+	dk := pbkdf2.Key(password, nonce, 4096, 32, sha1.New)
+
+	// crée un nouveau bloc de chiffrement AES en utilisant la clé dérivée
+	block, err := aes.NewCipher(dk)
+	if err != nil {
+		return err
+	}
+
+	asegm, err := cipher.NewGCM(block)
+	if err != nil {
+		return err
+	}
+
+	// dechiffre le texte chiffré en utilisant le nonce et AES-GCM
+	textFile, err := asegm.Open(nil, nonce, cipherText, nil)
+	if err != nil {
+		return err
+	}
+
+	outFile, err := os.Create(src)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	_, err = outFile.Write(textFile)
+	return err
+}
+
+func ValidFile(file string) bool {
+	// Vérifie l'existence du fichier
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -10,14 +10,17 @@ import (
 	"golang.org/x/term"
 )
 
+var (
+	colorReset = "\033[0m"
+	colorBlue  = "\033[34m"
+	colorRed   = "\033[31m"
+)
+
 func main() {
 	Option()
 }
 
 func Usage() {
-	colorReset := "\033[0m"
-	colorBlue := "\033[34m"
-
 	fmt.Println("Usage :")
 	fmt.Println(colorBlue, "Exemple pour lancer le mode encrypt : go run . encrypt /chemin/du/fichier/", colorReset)
 	fmt.Println(colorBlue, "Exemple pour lancer le mode decrypt : go run . decrypt /chemin/du/fichier/", colorReset)
@@ -25,6 +28,7 @@ func Usage() {
 
 func Option() {
 	if len(os.Args) < 3 {
+		fmt.Println(colorRed, "Erreur : Vous devez spécifier le mode (encrypt/decrypt) et le chemin du fichier.", colorReset)
 		Usage()
 		os.Exit(1)
 	}
@@ -37,6 +41,7 @@ func Option() {
 	case "decrypt":
 		DecryptFunc()
 	default:
+		fmt.Println(colorRed, "Erreur : Mode inconnu. Utilisez 'encrypt' ou 'decrypt'.", colorReset)
 		Usage()
 		os.Exit(1)
 	}
@@ -44,65 +49,80 @@ func Option() {
 
 func DecryptFunc() {
 	if len(os.Args) < 3 {
-		fmt.Println("Erreur : chemin de fichier manquant.")
+		fmt.Println(colorRed, "Erreur : Vous devez spécifier le chemin du fichier.", colorReset)
 		Usage()
 		os.Exit(1)
 	}
 	file := os.Args[2]
 
-	if !ValidFile(file) {
-		panic("Fichier non trouvé")
+	if !fcrypt.ValidFile(file) {
+		fmt.Println(colorRed, "Erreur : Fichier non trouvé.", colorReset)
+		os.Exit(1)
 	}
 
-	fmt.Printf("Entrez le mot de passe: ")
+	fmt.Printf("Entrez le mot de passe : ")
 	password, _ := term.ReadPassword(0)
-	fmt.Println("\nDéchiffrement en cours...")
 
-	fcrypt.(file, password)
-	fmt.Println("\nFichier déchiffré avec succès")
+	if len(password) == 0 {
+		fmt.Println(colorRed, "\nErreur : le mot de passe ne peut pas être vide.", colorReset)
+		os.Exit(1)
+	}
+
+	fmt.Println("\nDéchiffrement en cours...")
+	err := fcrypt.Decrypt(file, password)
+	if err != nil {
+		fmt.Println("Erreur lors du déchiffrement :", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("\nFichier déchiffré avec succès.")
 }
 
 func EncryptFunc() {
 	if len(os.Args) < 3 {
-		fmt.Println("Erreur : chemin de fichier manquant.")
+		fmt.Println(colorRed, "Erreur : Vous devez spécifier le chemin du fichier.", colorReset)
 		Usage()
 		os.Exit(1)
 	}
 	file := os.Args[2]
 
-	if !ValidFile(file) {
-		panic("Fichier non trouvé")
+	if !fcrypt.ValidFile(file) {
+		fmt.Println(colorRed, "Erreur : Fichier non trouvé.", colorReset)
+		os.Exit(1)
 	}
 
 	password := GetPassword()
-	fmt.Println("\nChiffrement en cours...")
 
-	fcrypt.Encrypt(file, password)
-	fmt.Println("\nFichier chiffré avec succès")
+	fmt.Println("\nChiffrement en cours...")
+	err := fcrypt.Encrypt(file, password)
+	if err != nil {
+		fmt.Println("Erreur lors du chiffrement :", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("\nFichier chiffré avec succès.")
 }
 
 func GetPassword() []byte {
-	fmt.Printf("Entrez le mot de passe: ")
+	fmt.Printf("Entrez le mot de passe : ")
 	password, _ := term.ReadPassword(0)
 
-	fmt.Printf("\nConfirmez le mot de passe: ")
+	if len(password) == 0 {
+		fmt.Printf(colorRed, "\nErreur : le mot de passe ne peut pas être vide.\n", colorReset)
+		return GetPassword()
+	}
+
+	fmt.Printf("\nConfirmez le mot de passe : ")
 	password2, _ := term.ReadPassword(0)
 
 	if !ValidPassword(password, password2) {
-		fmt.Printf("\nLes mots de passe ne correspondent pas. Réessayez !\n")
+		fmt.Printf(colorRed, "\nLes mots de passe ne correspondent pas. Réessayez !\n", colorReset)
 		return GetPassword()
 	}
+
 	return password
 }
 
 func ValidPassword(password []byte, password2 []byte) bool {
 	return bytes.Equal(password, password2)
-}
-
-func ValidFile(file string) bool {
-	// Vérifie l'existence du fichier
-	if _, err := os.Stat(file); os.IsNotExist(err) {
-		return false
-	}
-	return true
 }
